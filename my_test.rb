2b138a1b63d102e63f6ccace335bba77da8fb1eb
bootstraponline <code@bootstraponline.com>
2013-03-18 18:19:02 -0300

    Update my_test.rb

--- my_test.rb
+++ my_test.rb
@@ -3,7 +3,7 @@
 # run with
 # ruby my_test.rb
 #
-# Example use of running a test standalone.
+# Example use of running a test standalone. Useful for debugging a single test.
 
 $:.unshift File.expand_path('..', __FILE__)
 
