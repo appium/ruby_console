68b4b8085b95582528fe7769fff3682a88e161da
bootstraponline <code@bootstraponline.com>
2013-03-20 17:27:07 -0400

    Release 0.0.15

--- lib/ruby_console/version.rb
+++ lib/ruby_console/version.rb
@@ -1,5 +1,5 @@
 # encoding: utf-8
 module RubyConsole
-  VERSION = '0.0.14' unless defined? ::RubyConsole::VERSION
+  VERSION = '0.0.15' unless defined? ::RubyConsole::VERSION
   DATE = '2013-03-20' unless defined? ::RubyConsole::DATE
 end
