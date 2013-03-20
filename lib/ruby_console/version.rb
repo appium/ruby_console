6423db32896af21697071bd74ca867742384bb88
bootstraponline <code@bootstraponline.com>
2013-03-20 11:20:30 -0400

    Bump gemspec

--- lib/ruby_console/version.rb
+++ lib/ruby_console/version.rb
@@ -1,5 +1,5 @@
 # encoding: utf-8
 module RubyConsole
-  VERSION = '0.0.9' unless defined? ::RubyConsole::VERSION
-  DATE = '2013-03-19' unless defined? ::RubyConsole::DATE
+  VERSION = '0.0.10' unless defined? ::RubyConsole::VERSION
+  DATE = '2013-03-20' unless defined? ::RubyConsole::DATE
 end
