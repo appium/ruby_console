2050852fd299b18501a5b43f56b56df3f7cf63ac
bootstraponline <code@bootstraponline.com>
2013-03-22 20:10:06 -0400

    Fix gemspec

--- lib/appium_console/version.rb
+++ lib/appium_console/version.rb
@@ -1,5 +1,5 @@
 # encoding: utf-8
 module AppiumConsole
-  VERSION = '0.0.17' unless defined? ::AppiumConsole::VERSION
-  DATE = '2013-03-21' unless defined? ::AppiumConsole::DATE
+  VERSION = '0.0.18' unless defined? ::AppiumConsole::VERSION
+  DATE = '2013-03-22' unless defined? ::AppiumConsole::DATE
 end
