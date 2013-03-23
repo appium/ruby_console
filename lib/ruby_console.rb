900dd169ec57108b54e047df24274032d8f33753
bootstraponline <code@bootstraponline.com>
2013-03-22 20:06:40 -0400

    Restore files

---
+++ lib/ruby_console.rb
@@ -0,0 +1,8 @@
+require 'rubygems'
+require 'pry'
+require 'appium_lib'
+
+start = File.expand_path "../start.rb", __FILE__
+cmd = ['-r', start]
+$stdout.puts 'pry ' + cmd.join(' ')
+Pry::CLI.parse_options cmd
