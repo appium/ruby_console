94ae27d54c57f16b45937decfded0cdac9e10160
bootstraponline <code@bootstraponline.com>
2013-04-11 10:45:26 -0400

    Release 0.0.28

--- lib/appium_console/version.rb
+++ lib/appium_console/version.rb
@@ -1,5 +1,5 @@
 # encoding: utf-8
 module AppiumConsole
-  VERSION = '0.0.27' unless defined? ::AppiumConsole::VERSION
-  DATE = '2013-04-05' unless defined? ::AppiumConsole::DATE
+  VERSION = '0.0.28' unless defined? ::AppiumConsole::VERSION
+  DATE = '2013-04-11' unless defined? ::AppiumConsole::DATE
 end
