521b85e570717dc83481e4d2baf0804787031c46
bootstraponline <code@bootstraponline.com>
2013-04-26 19:39:51 -0400

    Release 0.3.2

--- lib/appium_console/version.rb
+++ lib/appium_console/version.rb
@@ -2,6 +2,6 @@
 # Define Appium module so version can be required directly.
 module Appium; end unless defined? Appium
 module Appium::Console
-  VERSION = '0.3.1' unless defined? ::Appium::Console::VERSION
+  VERSION = '0.3.2' unless defined? ::Appium::Console::VERSION
   DATE = '2013-04-26' unless defined? ::Appium::Console::DATE
 end
