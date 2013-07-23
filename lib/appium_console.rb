c36701b11f1d2eab4885a6492cb320d48e3c7d1f
bootstraponline <code@bootstraponline.com>
2013-07-23 16:43:37 -0400

    Require Ruby files listed in appium.txt
    appium.txt may contain a list of Ruby files to be required.
    Appium console will now load these files into Pry by default.
    
    Remember to only run appium.txt files that you trust.
    It's not safe to execute random Ruby code from the internet.

--- lib/appium_console.rb
+++ lib/appium_console.rb
@@ -7,10 +7,18 @@ module Appium; end unless defined? Appium
 
 module Appium::Console
   require 'appium_lib'
-  load_appium_txt file: Dir.pwd + '/appium.txt'
+  to_require = load_appium_txt file: Dir.pwd + '/appium.txt', verbose: true
 
   start = File.expand_path '../start.rb', __FILE__
+  # to do:
+  # -e switch to pass require string
   cmd = ['-r', start]
+
+  if to_require && !to_require.empty?
+    load_files = to_require.map { |f| %(require "#{f}";) }.join "\n"
+    cmd += [ '-e', load_files ]
+  end
+
   $stdout.puts "pry #{cmd.join(' ')}"
   Pry::CLI.parse_options cmd
 end # module Appium::Console
