fdc7749b7fd074b89643407a1cd4c7ba71ff15f2
bootstraponline <code@bootstraponline.com>
2013-08-21 10:18:42 -0400

    Load minitest by default

--- lib/start.rb
+++ lib/start.rb
@@ -3,3 +3,13 @@ require 'rubygems'
 require 'appium_lib'
 
 Appium::Driver.new(export_session: true).start_driver if $driver.nil?
+
+# Load minitest
+begin
+  require 'minitest'
+  require 'minitest/spec'
+  # set current_spec. fixes:
+  # NoMethodError: undefined method `assert_equal' for nil:NilClass
+  Minitest::Spec.new 'pry'
+rescue
+end
