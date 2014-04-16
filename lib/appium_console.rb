9b6b201f2d290f34e97b55b40d353b83686fa31b
bootstraponline <code@bootstraponline.com>
2014-04-16 12:11:12 -0400

    Update for new appium_lib

--- lib/appium_console.rb
+++ lib/appium_console.rb
@@ -1,42 +1,51 @@
 # encoding: utf-8
 require 'rubygems'
-Gem::Specification.class_eval { def self.warn( args ); end }
-require 'pry'
 
-module Appium; end unless defined? Appium
-
-def define_reload
-  Pry.send(:define_singleton_method, :reload) do
-    parsed = load_appium_txt file: Dir.pwd + '/appium.txt'
-    return unless parsed && parsed[:appium_lib] && parsed[:appium_lib][:requires]
-    requires = parsed[:appium_lib][:requires]
-    requires.each do |file|
-      # If a page obj is deleted then load will error.
-      begin
-        load file
-      rescue # LoadError: cannot load such file
-      end
-    end
+# Silence gem warnings
+Gem::Specification.class_eval do
+  def self.warn args
   end
-  nil
 end
 
-module Appium::Console
-  require 'appium_lib'
-  AwesomePrint.pry!
-  parsed = load_appium_txt file: Dir.pwd + '/appium.txt', verbose: true
+require 'pry'
+
+Pry.send(:define_singleton_method, :pry_load_appium_txt) do |opts={}|
+  verbose = opts.fetch :verbose, false
+  Appium.load_appium_txt file: Dir.pwd + '/appium.txt', verbose: verbose
+end
+
+Pry.send(:define_singleton_method, :reload) do
+  parsed = Pry.pry_load_appium_txt
   return unless parsed && parsed[:appium_lib] && parsed[:appium_lib][:requires]
-  to_require = parsed[:appium_lib][:requires]
-
-  start = File.expand_path '../start.rb', __FILE__
-  cmd = ['-r', start]
-
-  if to_require && !to_require.empty?
-    define_reload
-    load_files = to_require.map { |f| %(require "#{f}";) }.join "\n"
-    cmd += [ '-e', load_files ]
+  requires = parsed[:appium_lib][:requires]
+  requires.each do |file|
+    # If a page obj is deleted then load will error.
+    begin
+      load file
+    rescue # LoadError: cannot load such file
+    end
   end
+end
 
-  $stdout.puts "pry #{cmd.join(' ')}"
-  Pry::CLI.parse_options cmd
-end # module Appium::Console
+module Appium
+  module Console
+    require 'appium_lib'
+    AwesomePrint.pry!
+    start = File.expand_path '../start.rb', __FILE__
+    cmd   = ['-r', start]
+
+    parsed       = Pry.pry_load_appium_txt verbose: true
+    has_requires = parsed && parsed[:appium_lib] && parsed[:appium_lib][:requires]
+    if has_requires
+      requires = parsed[:appium_lib][:requires]
+
+      if !requires.empty?
+        load_files = requires.map { |f| %(require "#{f}";) }.join "\n"
+        cmd        += ['-e', load_files]
+      end
+
+      $stdout.puts "pry #{cmd.join(' ')}"
+    end
+    Pry::CLI.parse_options cmd
+  end
+end
--- lib/start.rb
+++ lib/start.rb
@@ -2,7 +2,7 @@
 require 'rubygems'
 require 'appium_lib'
 
-Appium::Driver.new(export_session: true).start_driver if $driver.nil?
+Appium::Driver.new(Pry.pry_load_appium_txt).start_driver if $driver.nil?
 
 # Load minitest
 begin
