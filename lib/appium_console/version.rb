5ae45566a767dce4efc0116a737975c4aaec98ea
Matthew Edwards <medwards@instructure.com>
2016-03-18 15:34:41 -0400

    Release 2.0.0

--- lib/appium_console/version.rb
+++ lib/appium_console/version.rb
@@ -2,6 +2,6 @@
 # Define Appium module so version can be required directly.
 module Appium; end unless defined? Appium
 module Appium::Console
-  VERSION = '1.0.4' unless defined? ::Appium::Console::VERSION
-  DATE = '2015-04-16' unless defined? ::Appium::Console::DATE
+  VERSION = '2.0.0' unless defined? ::Appium::Console::VERSION
+  DATE = '2016-03-18' unless defined? ::Appium::Console::DATE
 end
