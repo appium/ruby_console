1257eb7f53af3f4dc001480438cd5c3316d81259
Chris Kruger <chris.kruger@krugerheavyindustries.com>
2016-11-01 17:06:06 +0800

    support for xctest compatible version of appium

--- lib/appium_console/version.rb
+++ lib/appium_console/version.rb
@@ -2,6 +2,6 @@
 # Define Appium module so version can be required directly.
 module Appium; end unless defined? Appium
 module Appium::Console
-  VERSION = '2.0.2' unless defined? ::Appium::Console::VERSION
+  VERSION = '2.1.0' unless defined? ::Appium::Console::VERSION
   DATE = '2016-09-23' unless defined? ::Appium::Console::DATE
 end
