c9646914c2ddb8cd92129bd82ef57ce51ad69d3a
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2017-09-01 23:52:43 +0900

    require appium 9.6+

--- lib/start.rb
+++ lib/start.rb
@@ -13,7 +13,7 @@ if $driver.nil?
   # override command timeout so the server doesn't shut down after 60 seconds
   new_command_timeout = { caps: { newCommandTimeout: 999_999 }.merge(opts[:caps] || {}) }
   opts = opts.merge(new_command_timeout)
-  Appium::Driver.new(opts).start_driver
+  Appium::Driver.new(opts, true).start_driver
   Appium.promote_appium_methods Object
 end
 
