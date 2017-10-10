79556e2af7da58c1eff28cd966dca05ca38ccfb4
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2017-10-11 00:57:33 +0900

    Release 2.6.0

--- lib/appium_console/version.rb
+++ lib/appium_console/version.rb
@@ -2,6 +2,6 @@
 # Define Appium module so version can be required directly.
 module Appium; end unless defined? Appium
 module Appium::Console
-  VERSION = '2.5.0' unless defined? ::Appium::Console::VERSION
-  DATE = '2017-09-02' unless defined? ::Appium::Console::DATE
+  VERSION = '2.6.0' unless defined? ::Appium::Console::VERSION
+  DATE = '2017-10-11' unless defined? ::Appium::Console::DATE
 end
