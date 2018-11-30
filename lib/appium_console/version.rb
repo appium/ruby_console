510438cbf0dca2747923150e60c61e8f2e91e7ad
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2018-11-30 23:15:48 +0900

    Release 2.9.2

--- lib/appium_console/version.rb
+++ lib/appium_console/version.rb
@@ -2,6 +2,6 @@
 # Define Appium module so version can be required directly.
 module Appium; end unless defined? Appium
 module Appium::Console
-  VERSION = '2.9.1' unless defined? ::Appium::Console::VERSION
-  DATE = '2018-10-11' unless defined? ::Appium::Console::DATE
+  VERSION = '2.9.2' unless defined? ::Appium::Console::VERSION
+  DATE = '2018-11-30' unless defined? ::Appium::Console::DATE
 end
