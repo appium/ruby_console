f7e8d1869e1ca09e0434c299dcbe126be79a7e15
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2021-01-25 10:27:44 -0800

    Release 2.13.0

--- lib/appium_console/version.rb
+++ lib/appium_console/version.rb
@@ -2,7 +2,7 @@
 module Appium; end unless defined? Appium
 module Appium
   module Console
-    VERSION = '2.12.0'.freeze unless defined? ::Appium::Console::VERSION
-    DATE = '2019-02-08'.freeze unless defined? ::Appium::Console::DATE
+    VERSION = '2.13.0'.freeze unless defined? ::Appium::Console::VERSION
+    DATE = '2021-01-25'.freeze unless defined? ::Appium::Console::DATE
   end
 end
