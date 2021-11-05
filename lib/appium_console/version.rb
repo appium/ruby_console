266360dbd5d55e281b112bf1da9b5aeb41cc5c74
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2021-11-05 00:52:05 -0700

    chore: bump version to rc2

--- lib/appium_console/version.rb
+++ lib/appium_console/version.rb
@@ -4,7 +4,7 @@
 module Appium; end unless defined? Appium
 module Appium
   module Console
-    VERSION = '2.14.0.rc1' unless defined? ::Appium::Console::VERSION
-    DATE = '2021-11-02' unless defined? ::Appium::Console::DATE
+    VERSION = '2.14.0.rc2' unless defined? ::Appium::Console::VERSION
+    DATE = '2021-11-05' unless defined? ::Appium::Console::DATE
   end
 end
