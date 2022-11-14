eaab2a0f1947fe18d53e89b213609f70c23191db
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2022-11-13 23:50:47 -0800

    Release 3.1.0

--- lib/appium_console/version.rb
+++ lib/appium_console/version.rb
@@ -4,7 +4,7 @@
 module Appium; end unless defined? Appium
 module Appium
   module Console
-    VERSION = '3.0.0' unless defined? ::Appium::Console::VERSION
-    DATE = '2021-11-06' unless defined? ::Appium::Console::DATE
+    VERSION = '3.1.0' unless defined? ::Appium::Console::VERSION
+    DATE = '2022-11-13' unless defined? ::Appium::Console::DATE
   end
 end
