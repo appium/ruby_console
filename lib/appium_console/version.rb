85ad6d26326accbb849369fb59d80a2ee5baa1d4
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2024-01-25 13:33:34 -0800

    bump the ruby_lib version

--- lib/appium_console/version.rb
+++ lib/appium_console/version.rb
@@ -4,7 +4,7 @@
 module Appium; end unless defined? Appium
 module Appium
   module Console
-    VERSION = '4.0.0' unless defined? ::Appium::Console::VERSION
-    DATE = '2023-06-19' unless defined? ::Appium::Console::DATE
+    VERSION = '4.1.0' unless defined? ::Appium::Console::VERSION
+    DATE = '2024-01-25' unless defined? ::Appium::Console::DATE
   end
 end
